8ee1acbb1da54081dc9599deb2dea6ba78d2c009
MOHITTIHOM <mohitbhandari.1995@gmail.com>
2017-02-16 01:17:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,3 +17,18 @@ img.onclick = function(){
   img.style.marginLeft = '100px';  
 };
 
+var button = document.getElementById('counter');
+var counter = 0;  
+button.onclick=function(){
+    // make a request
+    
+    //  capture a response
+    
+    
+    // render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();    
+}
+
+
