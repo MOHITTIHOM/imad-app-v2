7f1f77fdec9eea0f341d07c9c9464bdd626e8eb4
MOHITTIHOM <mohitbhandari.1995@gmail.com>
2017-02-16 00:12:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,10 @@ var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
 
+//Move image
+
+var img= document.getElementById('tihom');
+img.onclick = function(){
+  img.style.marginLeft = '100px';  
+};
 
