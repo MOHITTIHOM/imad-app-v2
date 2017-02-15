db68e148c0065719e6a99fbd3979a125942bc66d
MOHITTIHOM <mohitbhandari.1995@gmail.com>
2017-02-16 01:34:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ button.onclick=function(){
         if(request.readyState == XMLHttpRequest.DONE){
             if(request.status === 200){
                 var counter = request.responseText;
-                var span = docoment.getElementById('count');
+                var span = document.getElementById('count');
                 span.innerHtml = counter.toString();
             }
         }
